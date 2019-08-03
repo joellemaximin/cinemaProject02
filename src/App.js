7cb748e9234b6d83843d1fbc513f245719f7b854
joellemaximin <joelle.maximin@gmail.com>
2019-08-03 15:41:21 +0200

    add components for starter

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <header className="">
+    
+      </header>
+    </div>
+  );
+}
+
+export default App;
