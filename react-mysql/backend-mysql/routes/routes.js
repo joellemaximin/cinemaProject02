2da9ac426b3ac828891ca6a1e0d2aa2bdba3b545
joellemaximin <joelle.maximin@gmail.com>
2019-09-10 12:04:28 +0200

    add server express & bdd mysql

---
+++ react-mysql/backend-mysql/routes/routes.js
@@ -0,0 +1,11 @@
+module.exports = function (app) {
+// 	  var ProductsController = require('../controllers/ProductsController');
+
+
+//   	//CRUD for products
+//     app.route("/products")
+//       .get(ProductsController.getproducts);
+//     app.route("/getVideo")
+//       .postProduct(ProductsController.getVideo);
+ 
+};
